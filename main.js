12c4f88121ccdfe3e10420ed9db6f6e6c0082897
metazo <metaz0412@gmail.com>
2022-06-12 12:33:25 +0200

    Initial commit

--- main.js
+++ main.js
@@ -23,3 +23,29 @@ const bgBrulant = "linear-gradient(to top, #ff0844 0%, #ffb199 100%)";
 const bgWin =
   "linear-gradient(-225deg, #231557 0%, #44107A 29%, #ff1361 67%, #fff800 100%)";
 const bgLoose = "linear-gradient(60deg, #29323c 0%, #485563 100%)"; 
+
+//Play
+
+const play = () => {
+
+    //nombres aléatoires
+    const randomNumber = Math.floor(Math.random() * 101);
+    const totalVies = 6;
+    let vies = totalVies;
+
+    console.log(randomNumber);
+
+    //Actualisation a chaque essai - TOUTE LA LOGIQUE
+    formulaire.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const valeurInput = parseInt(input.value);
+
+        if (valeurInput < 0 || valeurInput > 100) return;
+
+        if (valeurInput === randomNumber) {
+            body.style.backgroundImage = bgWin;
+            message.textContent = `BRAVO !!! Le nombre était bien ${randomNumber}`;
+            rejouerBtn.body.display = 'block';
+        }
+    })
+}
